d95efc9e46cc865147a2efafeec132f40c22dbcf
victorhbc <victorhugoboninic@gmail.com>
2020-09-29 11:09:17 -0300

    CRUD moovie creation

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import Navbar from "./components/navbar.component"
-import ExercisesList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercise.component";
-import CreateExercise from "./components/create-exercise.component";
+// import ExercisesList from "./components/exercises-list.component";
+// import EditExercise from "./components/edit-exercise.component";
+// import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 import EditUser from "./components/edit-user.component";
 import UpdateUser from "./components/update-user.component"
@@ -17,9 +17,10 @@ function App() {
       <Navbar />
       <div className="container">
       <br/>
-      <Route path="/" exact component={ExercisesList} />
+      <Route path="/" exact component={EditUser} />
+      {/* <Route path="/" exact component={ExercisesList} />
       <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
+      <Route path="/create" component={CreateExercise} /> */}
       <Route path="/user" component={CreateUser} />
       <Route path="/del" component={EditUser} />
       <Route path="/update" component={UpdateUser} />
